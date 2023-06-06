5c7b1f1b9d8310cc5b831582397d7f911bd4ff8b
Brnsmgspd <101212470+Brnsmgspd@users.noreply.github.com>
2023-06-06 19:09:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Comunidade Cristã Canaã";
+
+// document.querySelector("html").addEventListener("click", function (){
+//     alert("Ouch");
+// });
